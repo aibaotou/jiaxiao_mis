98edc9b9a4e2eaf8df198b620f53f4c29e663042
liuyang0728 <liuyang728@sina.cn>
2019-07-23 17:29:29 +0800

    添加 router

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 // import { createStore } from 'redux';
 // import todoApp from './reducers';
 
@@ -10,6 +10,7 @@ import zhCN from 'antd/es/locale-provider/zh_CN';
 import moment from 'moment';
 import 'moment/locale/zh-cn';
 import Home from './views/home';
+import Login from './views/login';
 import 'normalize.css';
 import './app.css';
 
@@ -19,7 +20,8 @@ function App() {
   return (
     <LocaleProvider locale={zhCN}>
       <Router>
-        <Home />
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
       </Router>
     </LocaleProvider>
   );
